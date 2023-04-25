91a35bfd09a990abe09ec08b8a0f032f57f453dd
GJackMills <66023315+GJackMills@users.noreply.github.com>
2023-04-25 17:08:44 +0100

    Added WebSerial and Neopixel functionality!

--- ESP32_SHRIMP_Main/src/main.cpp
+++ ESP32_SHRIMP_Main/src/main.cpp
@@ -6,6 +6,12 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <AsyncElegantOTA.h>
+#include <WebSerial.h> // Latest additon is webserial - 25/04/23
+#include <Adafruit_NeoPixel.h> // Finally added the only library that matters RGB LEDs - 25/04/23
+
+#define NUMPIXELS 1 // Only one NeoPixel on board as standard
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+#define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
 const char* ssid = "Tracy Island";
 const char* password = "Thunderbird4";
@@ -41,12 +47,13 @@ uint32_t button_mask = (1 << BUTTON_RIGHT) | (1 << BUTTON_DOWN) |
 
 #define IRQ_PIN   5 // See the SeeSaw example for IRQ_Pin options for other boards
 bool Seesaw; // Stores whether there is a Seesaw Joywing / Initialised or not.
+bool sendESPNOW = false; // turns ESPNOW messaging on or off
 
 // REPLACE WITH THE MAC Address of your receiver  -  we will use Gump as the controller and FruitOTS as the SHRIMP
 uint8_t FruitOTS_Address[] = {0x60, 0x55, 0xF9, 0xF5, 0xA4, 0xC4}; // This is FruitOTS MAC- the SHRIMP
 uint8_t Gump_Address[] =     {0x60, 0x55, 0xF9, 0xF5, 0xA3, 0x80}; // This is Gump MAC - the controller
 
-int incoming_Serial; // Stores incoming Serial Data
+int incoming_Serial; // Stores incoming WebSerial Data
 
 // Define variables to store XYZ readings from Adafruit SeeSaw (SS) Controller
 int ss_x;
@@ -90,10 +97,10 @@ struct_message incoming_Readings;
 
 esp_now_peer_info_t peerInfo;
 
-// Callback when data is sent
+// ESP-NOW Function: Callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+  WebSerial.print("\r\nLast Packet Send Status:\t");
+  WebSerial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
   if (status ==0){
     success = "Delivery Success :)";
   }
@@ -102,34 +109,69 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   }
 }
 
-// Callback when data is received
+// ESP-NOW Function: Callback when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   memcpy(&incoming_Readings, incomingData, sizeof(incoming_Readings));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
+  WebSerial.print("Bytes received: ");
+  WebSerial.println(len);
   incoming_Temperature = incoming_Readings.x;
   incoming_Humidity= incoming_Readings.y;
   //incoming_Pressure = incoming_Readings.pressure;
   //incoming_Battery = incoming_Readings.battery;
 }
+ 
+
+#define LED_BUILTIN 13
+// WebSerial Function: Receives and decodes serial messages from WebSerial
+void recvMsg(uint8_t *data, size_t len){
+  WebSerial.println("Received Data...");
+  String d = "";
+  for(int i=0; i < len; i++){
+    d += char(data[i]);
+  }
+  WebSerial.println(d);
+  if (d == "ON"){
+    pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+    WebSerial.println("LED ON");
+  }
+  else if (d == "OFF"){
+    pixels.clear();
+    WebSerial.println("LED OFF");
+  }
+  else if (d == "sendESPNOW") {
+    sendESPNOW = !sendESPNOW;
+    WebSerial.println("Toggled ESPNOW Messages!");
+  }
+  else if (d == "CMD"){
+    WebSerial.println("Commands go here... (sorry)");
+  }
+  else {
+    WebSerial.println("Type 'CMD' for list of commands");
+  }
+}
 
 
 void setup() {
-  Serial.begin(115200);
+  //Serial.begin(115200); // We use web serial now so this is defunct
 
-  set_SHRIMP_Name();
+  set_SHRIMP_Name(); // Function works out the SHRIMP hostname from MAC address
 
   WiFi.mode(WIFI_STA);
   WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE, INADDR_NONE);
-  WiFi.setHostname(hostname.c_str());
+  WiFi.setHostname(hostname.c_str()); // Sets the hostname of the SHRIMP
   WiFi.begin(ssid, password);
+
+   // Start WebSerial: WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  WebSerial.msgCallback(recvMsg); // Function that handles messages from WebSerial.
 
  
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    WebSerial.print(".");
   }
   
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -139,52 +181,41 @@ void setup() {
   AsyncElegantOTA.begin(&server);    // Start ElegantOTA
   server.begin();
   
-   while (!Serial){ // Stop until serial is connected
-  delay(10);
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("HTTP server started, Hostname: ");
-  Serial.println(hostname);
+   //while (!WebSerial){ // Stop until serial is connected - comment out to stop waiting for usb serial
+  //delay(10);
+  //}
+
+  WebSerial.println("");
+  WebSerial.print("Connected to ");
+  WebSerial.println(ssid);
+  WebSerial.print("IP address: ");
+  WebSerial.println(WiFi.localIP());
+  WebSerial.print("HTTP server started, Hostname: ");
+  WebSerial.println(hostname);
 
   // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   // Now setup ESP-NOW with the Seesaw controller
-
   // Init Adafruit SeeSaw Joy
-  bool While_Seesaw = true; // Just a while loop condition for attaching the Seesaw
-  do {
-      if(!ss.begin(0x49)){
-        Serial.println("ERROR! Seesaw Joywing Controller not found!");
-        Serial.println("Check Seesaw Joywing Controller connection or type 'Y' to continue without Seesaw");
-        if(Serial.available() > 0) {     // read the incoming byte:
-          incoming_Serial = Serial.read(); 
-        }
-        if (incoming_Serial == 89){
-          Serial.println("OK, starting without Seesaw!");
-          Seesaw = false;
-          While_Seesaw = false;
-        }
-      }
-      else {
-        Serial.println("Seesaw Joywing Controller started!");
-        Serial.print("Version: ");
-        Serial.println(ss.getVersion(), HEX);
-        Seesaw = true;
-        While_Seesaw = false;
-      }
-  } while (While_Seesaw); // Keep trying to initialise the Seesaw until user presses continue:  Y in ASCII = 89
+  if(!ss.begin(0x49)){
+    WebSerial.println("ERROR! Seesaw Joywing controller not found!");
+    WebSerial.println("Starting without Seesaw!");
+    Seesaw = false;
+  }
+  else {
+    WebSerial.println("Seesaw Joywing controller started!");
+    WebSerial.print("Version: ");
+    WebSerial.println(ss.getVersion(), HEX);
+    Seesaw = true;
+  }
 
-  ss.pinModeBulk(button_mask, INPUT_PULLUP);
-  ss.setGPIOInterrupts(button_mask, 1);
+  ss.pinModeBulk(button_mask, INPUT_PULLUP); // Activate Seesaw buttons or something
+  ss.setGPIOInterrupts(button_mask, 1); // Activate Seesaw interrupts?
 
   pinMode(IRQ_PIN, INPUT); // Interrupt pin
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
+    WebSerial.println("Error initializing ESP-NOW");
     return;
   }
 
@@ -204,14 +235,16 @@ void setup() {
   
   // Add peer        
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
-    Serial.println("Failed to add peer");
+    WebSerial.println("Failed to add peer");
     return;
   }
   // Register for a callback function that will be called when data is received
   esp_now_register_recv_cb(OnDataRecv);
 
-
+  pixels.begin(); // INITIALIZE NeoPixel strip object
 }
+
+
 
 int last_x = 0;
 int last_y = 0;
@@ -221,7 +254,7 @@ void Read_Seesaw(){
   ss_y = ss.analogRead(3);
   
   if ( (abs(ss_x - last_x) > 3)  ||  (abs(ss_y - last_y) > 3)) {
-    Serial.print(ss_x); Serial.print(", "); Serial.println(ss_y);
+    WebSerial.print(ss_x); WebSerial.print(", "); WebSerial.println(ss_y);
     last_x = ss_x;
     last_y = ss_y;
   }
@@ -230,22 +263,22 @@ void Read_Seesaw(){
 
     uint32_t buttons = ss.digitalReadBulk(button_mask);
 
-    //Serial.println(buttons, BIN);
+    //WebSerial.println(buttons, BIN);
 
     if (! (buttons & (1 << BUTTON_RIGHT))) {
-      Serial.println("Button A pressed");
+      WebSerial.println("Button A pressed");
     }
     if (! (buttons & (1 << BUTTON_DOWN))) {
-      Serial.println("Button B pressed");
+      WebSerial.println("Button B pressed");
     }
     if (! (buttons & (1 << BUTTON_LEFT))) {
-      Serial.println("Button Y pressed");
+      WebSerial.println("Button Y pressed");
     }
     if (! (buttons & (1 << BUTTON_UP))) {
-      Serial.println("Button X pressed");
+      WebSerial.println("Button X pressed");
     }
     if (! (buttons & (1 << BUTTON_SEL))) {
-      Serial.println("Button SEL pressed");
+      WebSerial.println("Button SEL pressed");
     }
   /* } // Uncomment to use IRQ */
   //delay(10);
@@ -262,29 +295,30 @@ void loop() {
   ss_Readings.y = ss_y;
 
   // Send message via ESP-NOW
-  Serial.println(hostname);
-  if (hostname == "ESP32-Gump") { // If this is Gump
+  
+  if (sendESPNOW == true && hostname == "ESP32-Gump") { // If this is Gump
     esp_err_t result = esp_now_send(FruitOTS_Address, (uint8_t *) &ss_Readings, sizeof(ss_Readings)); // Send a message to FruitOTS
     if (result == ESP_OK) {
-      Serial.println("Sent from Gump to FruitOTS with success!");
+      //WebSerial.println("Sent from Gump to FruitOTS");
     } else {
-      Serial.println("Error sending the data to FruitOTS");
+      //WebSerial.println("Error sending the data to FruitOTS");
     }
   }
-  else if (hostname == "ESP32-FruitOTS") { // Else if this is FruitOTS 
+  else if (sendESPNOW == true && hostname == "ESP32-FruitOTS") { // Else if this is FruitOTS 
     esp_err_t result = esp_now_send(Gump_Address, (uint8_t *) &ss_Readings, sizeof(ss_Readings)); // Send a message to Gump
     if (result == ESP_OK) {
-      Serial.println("Sent from FruitOTS to Gump with success!");
+      //WebSerial.println("Sent from FruitOTS to Gump");
     } else {
-      Serial.println("Error sending the data to Gump");
+      //WebSerial.println("Error sending the data to Gump");
     }
   }
    
   incoming_x = incoming_Readings.x;
   incoming_y = incoming_Readings.y;
 
-  Serial.println(incoming_x);
-  Serial.println(incoming_y);
-
+  //WebSerial.println(incoming_x);
+  //WebSerial.println(incoming_y);
   delay(1000);
 }
+
+
