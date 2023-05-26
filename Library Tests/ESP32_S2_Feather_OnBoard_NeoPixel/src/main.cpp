a61102fcb4b50d6030b330a3920075cceff8e5a9
GJackMills <66023315+GJackMills@users.noreply.github.com>
2023-05-26 17:15:24 +0100

    Friday update, not sure what I changed...

--- Library Tests/ESP32_S2_Feather_OnBoard_NeoPixel/src/main.cpp
+++ Library Tests/ESP32_S2_Feather_OnBoard_NeoPixel/src/main.cpp
@@ -8,13 +8,17 @@
 #endif
 
 // How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 1 // Popular NeoPixel ring size
+#define NUMPIXELS 2 // Popular NeoPixel ring size
+#define NEOPIXEL_EXTERNAL 12
+
+bool onboard = false;
 
 // When setting up the NeoPixel library, we tell it how many pixels,
 // and which pin to use to send signals. Note that for older NeoPixel
 // strips you might need to change the third parameter -- see the
 // strandtest example for more information on possible values.
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel pixels(NUMPIXELS, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800); 
+Adafruit_NeoPixel strip(NUMPIXELS, NEOPIXEL_EXTERNAL, NEO_GRB + NEO_KHZ800);
 
 #define DELAYVAL 500 // Time (in milliseconds) to pause between pixels
 
@@ -26,22 +30,53 @@ void setup() {
 #endif
   // END of Trinket-specific code.
 
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  //pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+}
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
 }
 
 void loop() {
-  pixels.clear(); // Set all pixel colors to 'off'
+  //pixels.clear(); // Set all pixel colors to 'off'
+  strip.clear(); // Set all pixel colors to 'off'
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
+  if(onboard){
   for(int i=0; i<NUMPIXELS; i++) { // For each pixel...
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-    pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    //pixels.setPixelColor(i, pixels.Color(0, 150, 0));
+    strip.setPixelColor(i, strip.Color(0, 150, 0));
 
-    pixels.show();   // Send the updated pixel colors to the hardware.
+    //pixels.show();   // Send the updated pixel colors to the hardware.
+    strip.show();
 
     delay(DELAYVAL); // Pause before next pass through loop
   }
 }
+
+  rainbow(100);             // Flowing rainbow cycle along the whole strip
+}
+
+
+
